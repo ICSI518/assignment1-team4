35fff294b3c2cf1268b847f975bb9ee4ccb94038
Raja11-ctrl <mrrreddy11@gmail.com>
2024-09-23 17:15:01 -0400

    Updated login and signup .js files

--- finwise/backend/tables/login.js
+++ finwise/backend/tables/login.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 //LOGIN Schema
 
 const LoginSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
   });
 
--- finwise/backend/tables/signup.js
+++ finwise/backend/tables/signup.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
-// Register Schema
-const registerSchema = new mongoose.Schema({
+const signupSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  username: {ype: String, required: true, unique: true},
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  confirmPassword: { type: String, required: true }
 });
 
-const Register = mongoose.model('Register', registerSchema);
+const Signup = mongoose.model('Signup', signupSchema);
+
+module.exports = Signup;
